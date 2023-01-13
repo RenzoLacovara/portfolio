0ab19245bc09640ed8bf854c8565658b3f208de8
Renzo Lacovara <rnlacovara@gmail.com>
2023-01-13 02:15:11 -0300

    fade in animation added

--- script/script.js
+++ script/script.js
@@ -82,3 +82,20 @@ window.addEventListener("scroll", function () {
 function toggle() {
   document.querySelector("#burg").classList.remove("is-active");
 }
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
