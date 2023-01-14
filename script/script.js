04445e5fc8428b8f411bb6713a49e7a2099c3a6d
Renzo Lacovara <rnlacovara@gmail.com>
2023-01-14 03:54:16 -0300

    dark mode removed

--- script/script.js
+++ script/script.js
@@ -3,53 +3,6 @@ hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("is-active");
 });
 
-const darkMode = () => {
-  const body = document.querySelector("body");
-  const systemDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
-  const checkbox = document.querySelector("#mode");
-  let theme = window.localStorage.getItem("theme");
-  let preference;
-  const preferencia = () => {
-    if (systemDarkMode.matches) {
-      preference = "dark";
-    } else {
-      preference = "light";
-    }
-  };
-  const setPreferencia = (e) => {
-    if (e.target.checked == true) {
-      preference = "dark";
-      window.localStorage.setItem("theme", "dark");
-    } else if (e.target.checked == false) {
-      preference = "light";
-      window.localStorage.setItem("theme", "light");
-    }
-    setTheme(preference);
-  };
-  const setTheme = (pref) => {
-    if (pref === "dark") {
-      checkbox.checked = true;
-      body.classList.add("oscuro");
-    } else {
-      checkbox.checked = false;
-      body.classList.remove("oscuro");
-    }
-  };
-  const estadoInicial = () => {
-    if (theme === "dark") {
-      checkbox.checked = true;
-      body.classList.add("oscuro");
-    } else {
-      checkbox.checked = false;
-      body.classList.remove("oscuro");
-    }
-  };
-  checkbox.addEventListener("click", setPreferencia);
-  window.addEventListener("DOMContentLoaded", preferencia);
-  window.addEventListener("DOMContentLoaded", estadoInicial);
-};
-darkMode();
-
 var modal = document.getElementById("miModal");
 var modalImg = document.getElementById("modalzoom");
 const imgs = document.getElementsByClassName("imagen");
@@ -68,17 +21,22 @@ function cerrarimg() {
     modalzoom.className = "modal-content";
   }, 400);
 }
-var lastScrollTop;
-goup = document.getElementById("goup");
-window.addEventListener("scroll", function () {
-  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  if (scrollTop > lastScrollTop) {
-    goup.classList.add("visible");
+
+let mybutton = document.getElementById("goup");
+window.onscroll = function () {
+  scrollFunction();
+};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.opacity = "1";
   } else {
-    goup.classList.remove("visible");
+    mybutton.style.opacity = "0";
   }
-  lastScrollTop = scrollTop;
-});
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 function toggle() {
   document.querySelector("#burg").classList.remove("is-active");
 }
