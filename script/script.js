eb346c3f60d589e657c237b5cc66e8e8c3bd8835
Renzo Lacovara <rnlacovara@gmail.com>
2023-03-10 01:23:53 -0300

    actualizacion

--- script/script.js
+++ script/script.js
@@ -54,6 +54,19 @@ function reveal() {
       reveals[i].classList.remove("active");
     }
   }
+  var reveals2 = document.querySelectorAll(".reveal2");
+
+  for (var i = 0; i < reveals2.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals2[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals2[i].classList.add("active");
+    } else {
+      reveals2[i].classList.remove("active");
+    }
+  }
 }
 
 window.addEventListener("scroll", reveal);
