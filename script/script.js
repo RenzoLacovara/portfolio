86f48306222ecb99dd21077d7e3e78a7cd18d1df
Renzo Lacovara <rnlacovara@gmail.com>
2023-03-29 22:06:12 -0300

    animations added

--- script/script.js
+++ script/script.js
@@ -40,43 +40,6 @@ function topFunction() {
 function toggle() {
   document.querySelector("#burg").classList.remove("is-active");
 }
-function reveal() {
-  var reveals = document.querySelectorAll(".reveal");
-
-  var windowHeight = window.innerHeight;
-  var elementVisible = 150;
-  for (var i = 0; i < reveals.length; i++) {
-    var elementTop = reveals[i].getBoundingClientRect().top;
-
-    if (elementTop < windowHeight - elementVisible) {
-      reveals[i].classList.add("active");
-    } else {
-      reveals[i].classList.remove("active");
-    }
-  }
-  var reveals2 = document.querySelectorAll(".reveal2");
-
-  for (var i = 0; i < reveals2.length; i++) {
-    var elementTop = reveals2[i].getBoundingClientRect().top;
-    if (elementTop < windowHeight - elementVisible) {
-      reveals2[i].classList.add("active");
-    } else {
-      reveals2[i].classList.remove("active");
-    }
-  }
-  var reveals3 = document.querySelectorAll(".reveal3");
-
-  for (var i = 0; i < reveals3.length; i++) {
-    var elementTop = reveals3[i].getBoundingClientRect().top;
-    if (elementTop < windowHeight - elementVisible) {
-      reveals3[i].classList.add("active");
-    } else {
-      reveals3[i].classList.remove("active");
-    }
-  }
-}
-
-window.addEventListener("scroll", reveal);
 
 $(document).ready(function () {
   var $sm = 660;
@@ -115,3 +78,20 @@ $(document).ready(function () {
     });
   });
 });
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
